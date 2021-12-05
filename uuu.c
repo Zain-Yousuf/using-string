c587123049fc8ae55df7d580a9b0fdf7b29beb01
Zain-Yousuf <93894509+Zain-Yousuf@users.noreply.github.com>
2021-12-05 11:34:25 +0500

    Add files via upload

---
+++ uuu.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+   char greeting[11]={'h','e','l','l','o','\n','z','a','i','n','\0'};
+   printf("greeting masege : %s\n",greeting);
+   return 0;
+
+}
